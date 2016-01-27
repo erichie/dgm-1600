c7324b226f6997775be6bc4ae9da5d2fdacbebef
Edward Richards <e4.rich87@gmail.com>
2016-01-26 18:04:19 -0700

    Branch and Merge Assignment

---
+++ Assets/Scripts/BranchMerge.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BranchMerge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Put code here
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Put code here
+	}
+
+	void movePlayer() {
+		// Put code here
+	}
+}
