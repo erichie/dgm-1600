6310a6eaf5d620e9a574ef9d8483df1225af8cbd
Edward Richards <e4.rich87@gmail.com>
2016-01-26 17:23:07 -0700

    Variables and Functions Assignment

---
+++ Assets/Scripts/VariablesFunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesFunctions : MonoBehaviour {
+	int sum;
+
+	// Use this for initialization
+	void Start () {
+		sum = addTwoNumbers (7, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	int addTwoNumbers(int x, int y) {
+		int sum = x + y;
+		return sum;
+	}
+}
