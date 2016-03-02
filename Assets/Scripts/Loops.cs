bfa686159d38eddfd17416f0ec8ce8e81346871c
Edward Richards <e4.rich87@gmail.com>
2016-03-01 17:48:58 -0700

    loops assignment

---
+++ Assets/Scripts/Loops.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+	public int age;
+	private int deathAge = 100;
+	private int[] ageOfFriends = {10, 14, 12, 17};
+
+	void Start() {
+		while (age < deathAge) {
+			Debug.Log ("Player is alive.");
+			age++;
+		}
+		Debug.Log ("Player is dead.");
+
+		do {
+			Debug.Log (age);
+		} while(age < deathAge);
+
+		for (int i = 50; i < deathAge; i++) {
+			Debug.Log ("Player is awesome");
+		}
+
+		foreach (int ages in ageOfFriends) {
+			Debug.Log (ages);
+		}
+	}
+}
