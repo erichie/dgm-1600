6ccfb5764de2f938ad9107ae697d3db10377387d
Edward Richards <e4.rich87@gmail.com>
2016-03-01 17:48:34 -0700

    if statements assignment

---
+++ Assets/Scripts/IfStatements.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+	
+	private bool walking;
+	private int runningSpeed = 50;
+
+	public int speed;
+
+	void Update()
+	{
+		if (speed == 0) {
+			Debug.Log ("Player is standing.");
+		} else if (speed > 0 && speed < runningSpeed) {
+			Debug.Log ("Player is walking");
+		} else if (speed == runningSpeed) {
+			Debug.Log ("Player is running");
+		} else {
+			Debug.Log("Player is dead");
+		}
+	}
+}
