c48aa20f13f57253a8269e643e4285203fbc01bb
Edward Richards <e4.rich87@gmail.com>
2016-04-14 17:34:32 -0600

    Arrays assignment

---
+++ Assets/Scripts/Arrays.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+	// Declaring an array
+	public int[] numbers;
+
+	// Declaring and initializing an array on the same line
+	public string[] names = {"Edward", "Frank", "Joe", "Bruce", "Steve"};
+
+	// Use this for initialization
+	void Start () {
+		// Initializing an array
+		numbers = new int[10];
+
+		// Accessing an item in the array
+		Debug.Log (names[0]);
+
+		// Using a for loop to add items to an array
+		for (int i = 0; i < 10; i++) {
+			numbers[i] = i * 2;
+		}
+
+		// Using a for loop to print the items of an array
+		for (int i = 0; i < numbers.Length; i++) {
+			Debug.Log (numbers[i]);
+		}
+	}
+}
