96b7f091eaa428e52a9e2edd1c6dbd4e9a658ee0
Edward Richards <e4.rich87@gmail.com>
2016-04-14 17:34:01 -0600

    Classes and Data Types assignment

---
+++ Assets/Scripts/ClassesDataTypes.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClassesDataTypes : MonoBehaviour {
+
+	// Creating a class
+	public class Enemy {
+
+		// Default Constructor
+		public Enemy() {
+			Debug.Log ("Enemy Constructor.");
+		}
+		
+		public void attack() {
+			Debug.Log ("Enemy Attack!!!");
+		}
+	}
+
+	// Creating a sub class
+	public class Goblin : Enemy {
+		
+		public int health;
+
+		// Contstructor with parameters
+		public Goblin(int goblinHealth) {
+			health = goblinHealth;
+		}
+		
+		public void goblinAttack() {
+			Debug.Log ("Goblin Attack!!!");
+		}
+
+		public void showGoblinHealth() {
+			Debug.Log (health);
+		}
+	}
+
+	// Creating instances of a class
+	public Enemy enemy = new Enemy();
+	public Goblin goblin = new Goblin(100);
+	
+	void Start () {
+		enemy.attack();
+		goblin.goblinAttack ();
+		goblin.showGoblinHealth ();
+	}
+}
