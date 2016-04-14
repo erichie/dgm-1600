6e16bf7bfe7860ec8381cb74078c47a5c7bbf2b8
Edward Richards <e4.rich87@gmail.com>
2016-04-14 17:35:21 -0600

    Switch assignment

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public int myCase = 19;
+
+	// Use this for initialization
+	void Start () {
+
+		// Using a switch statement
+		switch(myCase) {
+			case 1:
+				Debug.Log ("My favorite number is 1!");
+				break;
+			case 4:
+				Debug.Log ("My favorite number is 4!");
+				break;
+			default:
+				Debug.Log ("I don't have a favorite number!");
+				break;
+		}
+	}
+}
